a851dbe937b3e0eab473a361eb0d2a58913c7906
Charles Pence <charles@charlespence.net>
2015-05-21 07:30:34 -0500

    Fix bug in Puma config.

--- deploy/roles/web/files/puma.rb
+++ deploy/roles/web/files/puma.rb
@@ -7,7 +7,7 @@ state_path '/opt/bluepill/puma.state'
 
 stdout_redirect '/opt/rletters/root/log/puma.stdout.log', '/opt/rletters/root/log/puma.stderr.log'
 
-bind '/opt/rletters/root/tmp/sockets/puma.sock'
+bind 'unix:///opt/rletters/root/tmp/sockets/puma.sock'
 
 workers 2
 threads 0, 5
